1e288c9b9c20033e7be58bff532b400705208445
Sergey Levko <okvel.s@gmail.com>
2020-10-24 20:31:42 +0300

    PN-3 - Update database scheme and initialization script

--- database/init.sql
+++ database/init.sql
@@ -6,28 +6,66 @@ CREATE TABLE "user" (
 
 CREATE TABLE note (
 	id BIGSERIAL PRIMARY KEY NOT NULL,
-	name TEXT,
+	title VARCHAR(255) NOT NULL,
 	created_at TIMESTAMP NOT NULL,
 	updated_at TIMESTAMP NOT NULL,
-	user_id BIGSERIAL references "user"(id)
+	pinned BOOLEAN NOT NULL DEFAULT false,
+	user_id BIGSERIAL NOT NULL references "user"(id)
 );
 
-CREATE TABLE content_type (
+CREATE TABLE note_text (
 	id BIGSERIAL PRIMARY KEY NOT NULL,
-	type VARCHAR(255) NOT NULL UNIQUE
-);
-
-CREATE TABLE note_content (
-	id BIGSERIAL PRIMARY KEY NOT NULL,
-	type_id BIGSERIAL references content_type(id),
-	note_id BIGSERIAL references note(id),
 	text TEXT,
+	note_id BIGSERIAL NOT NULL references note(id)
+);
+
+CREATE TABLE task_group (
+	id BIGSERIAL PRIMARY KEY NOT NULL,
+	name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE task (
+	id BIGSERIAL PRIMARY KEY NOT NULL,
+	title TEXT NOT NULL,
+	description TEXT,
 	created_at TIMESTAMP NOT NULL,
-	updated_at TIMESTAMP NOT NULL
+	updated_at TIMESTAMP NOT NULL,
+	pinned BOOLEAN NOT NULL DEFAULT false,
+	execute_at TIMESTAMP,
+	notify_at TIMESTAMP,
+	note_id BIGSERIAL NOT NULL references note(id),
+	group_id BIGSERIAL references task_group(id)
+);
+
+CREATE TABLE set_item (
+	id BIGSERIAL PRIMARY KEY NOT NULL,
+	name TEXT NOT NULL,
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL,
+	pinned BOOLEAN NOT NULL DEFAULT false,
+	note_id BIGSERIAL NOT NULL references note(id)
 );
 
 CREATE INDEX note_user_id_idx
-	ON note (user_id);
+	ON note(user_id);
 	
-CREATE INDEX note_content_note_id_idx
-	ON note_content (note_id);
+CREATE INDEX note_title_idx
+	ON note(title);
+	
+CREATE INDEX note_text_note_id_idx
+	ON note_text(note_id);
+	
+CREATE INDEX task_note_id_idx
+	ON task(note_id);
+	
+CREATE INDEX task_title_idx
+	ON task(title);
+	
+CREATE INDEX task_execute_at_idx
+	ON task(execute_at);
+	
+CREATE INDEX set_item_note_id_idx
+	ON set_item(note_id);
+	
+CREATE INDEX set_item_name_idx
+	ON set_item(name);
